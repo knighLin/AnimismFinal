9ee7109f48d194004f00026bf7f491a4c89c3d65
DESKTOP-06H8672\user <yxtji3@gmail.com>
2018-03-25 05:42:10 +0800

    不要再壞掉了 shit

---
+++ Assets/Script/UI/BringBring.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class BringBring : MonoBehaviour {
+    private float abc;
+    private bool def=true;
+    [SerializeField] private Image ghi;
+    [SerializeField] private GameObject jkl;
+    [SerializeField] private Button mno;
+
+
+    void Update () {
+        if (def)
+        {
+            abc += Time.timeScale;
+            ghi.color = new Color(1, 1, 1, abc / 40);
+        }
+        else
+        {
+            abc -= Time.timeScale;
+            ghi.color = new Color(1, 1, 1, abc / 40);
+        }
+        if (abc >=40)
+        {
+            def = false;
+        }
+        else if (abc<=0)
+        {
+            def = true;
+        }
+
+        if (Input.GetButtonDown("SquareAttack")|| Input.GetButtonDown("TriangleAbility")|| Input.GetButtonDown("CircleUnpossess")|| Input.GetButtonDown("CrossJump")|| Input.GetButtonDown("R1Locking")|| Input.GetButtonDown("OptionsCacel")|| Input.GetButtonDown("R1Locking")|| Input.GetButtonDown("R2Run")|| Input.GetButtonDown("L1SoulVison")|| Input.GetButtonDown("R2FixCamera"))
+        {
+            jkl.SetActive(true);
+            mno.Select();
+            this.gameObject.SetActive(false);
+        }
+
+	}
+}
